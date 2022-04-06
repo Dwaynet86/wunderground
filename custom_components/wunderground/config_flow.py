8df0dffeaa375fb7326cd21d359e1ea7f75a16c5
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2022-04-06 12:35:04 -0500

    Update config_flow.py

--- custom_components/wunderground/config_flow.py
+++ custom_components/wunderground/config_flow.py
@@ -40,7 +40,8 @@ class WuFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         )
         if user_input is not None:
             # Use the user-supplied API key and station id.
-            return self.async_setup_entry()
+            self.data = user_input
+            return self.async_create_entry(title="WUnderground PWS", data=self.data)
 
        
         return self.async_show_form(
