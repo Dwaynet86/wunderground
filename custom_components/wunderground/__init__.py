61dcf476219b7155ae59a4d1f7fc2bd6ae4f4301
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2022-04-06 09:29:43 -0500

    Update __init__.py

--- custom_components/wunderground/__init__.py
+++ custom_components/wunderground/__init__.py
@@ -1,7 +1,7 @@
 """The wunderground pws component."""
 import asyncio
 from datetime import timedelta
-import logging
+
 import re
 
 import aiohttp
@@ -17,20 +17,12 @@ from homeassistant.const import (
     LENGTH_FEET, LENGTH_MILLIMETERS, LENGTH_METERS, SPEED_MILES_PER_HOUR, SPEED_KILOMETERS_PER_HOUR,
     PERCENTAGE, PRESSURE_INHG, PRESSURE_MBAR, PRECIPITATION_INCHES_PER_HOUR, PRECIPITATION_MILLIMETERS_PER_HOUR,
     ATTR_ATTRIBUTION)
+import .const
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
-
-_RESOURCECURRENT = 'https://api.weather.com/v2/pws/observations/current?stationId={}&format=json&units={}&apiKey={}'
-_RESOURCEFORECAST = 'https://api.weather.com/v3/wx/forecast/daily/5day?geocode={},{}&units={}&{}&format=json&apiKey={}'
-_LOGGER = logging.getLogger(__name__)
-
-CONF_ATTRIBUTION = "Data provided by the WUnderground weather service"
-CONF_PWS_ID = 'pws_id'
-CONF_NUMERIC_PRECISION = 'numeric_precision'
-CONF_LANG = 'lang'
 
 
 
