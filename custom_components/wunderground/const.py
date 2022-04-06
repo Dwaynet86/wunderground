c43d7d800a5f2a806c7c9bf69dc6f915acba333c
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2022-04-06 09:12:53 -0500

    Update const.py

--- custom_components/wunderground/const.py
+++ custom_components/wunderground/const.py
@@ -23,6 +23,7 @@ DOMAIN = "wunderground"
 DATA_WU_CONFIG = "wunderground_config"
 CONF_LANG = "lang"
 CONF_PWS_ID = "pws_id"
+DEFAULT_LANG = 'en-US'
 
 PLATFORMS = [
     Platform.BINARY_SENSOR,
@@ -30,4 +31,14 @@ PLATFORMS = [
     Platform.HUMIDIFIER,
     Platform.SENSOR,
     Platform.WEATHER,
+]
+
+# Language Supported Codes
+LANG_CODES = [
+    'ar-AE', 'az-AZ', 'bg-BG', 'bn-BD', 'bn-IN', 'bs-BA', 'ca-ES', 'cs-CZ', 'da-DK', 'de-DE', 'el-GR', 'en-GB', 'en-IN',
+    'en-US', 'es-AR', 'es-ES', 'es-LA', 'es-MX', 'es-UN', 'es-US', 'et-EE', 'fa-IR', 'fi-FI', 'fr-CA', 'fr-FR', 'gu-IN',
+    'he-IL', 'hi-IN', 'hr-HR', 'hu-HU', 'in-ID', 'is-IS', 'it-IT', 'iw-IL', 'ja-JP', 'jv-ID', 'ka-GE', 'kk-KZ', 'kn-IN',
+    'ko-KR', 'lt-LT', 'lv-LV', 'mk-MK', 'mn-MN', 'ms-MY', 'nl-NL', 'no-NO', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU',
+    'si-LK', 'sk-SK', 'sl-SI', 'sq-AL', 'sr-BA', 'sr-ME', 'sr-RS', 'sv-SE', 'sw-KE', 'ta-IN', 'ta-LK', 'te-IN', 'tg-TJ',
+    'th-TH', 'tk-TM', 'tl-PH', 'tr-TR', 'uk-UA', 'ur-PK', 'uz-UZ', 'vi-VN', 'zh-CN', 'zh-HK', 'zh-TW'
 ]
