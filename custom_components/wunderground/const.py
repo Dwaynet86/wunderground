05565eb87c84c64ff05264ded1fca87d554725ed
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2022-04-05 21:26:00 -0500

    Create const.py

---
+++ custom_components/wunderground/const.py
@@ -0,0 +1,34 @@
+"""Constants for the wunderground pws integration."""
+
+import logging
+
+from homeassistant.components.weather import (
+    ATTR_CONDITION_CLOUDY,
+    ATTR_CONDITION_FOG,
+    ATTR_CONDITION_HAIL,
+    ATTR_CONDITION_LIGHTNING_RAINY,
+    ATTR_CONDITION_PARTLYCLOUDY,
+    ATTR_CONDITION_POURING,
+    ATTR_CONDITION_RAINY,
+    ATTR_CONDITION_SNOWY,
+    ATTR_CONDITION_SNOWY_RAINY,
+    ATTR_CONDITION_SUNNY,
+    ATTR_CONDITION_WINDY,
+)
+from homeassistant.const import Platform
+
+_LOGGER = logging.getLogger(__package__)
+
+DOMAIN = "ecobee"
+DATA_ECOBEE_CONFIG = "ecobee_config"
+
+CONF_INDEX = "index"
+CONF_REFRESH_TOKEN = "refresh_token"
+
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.CLIMATE,
+    Platform.HUMIDIFIER,
+    Platform.SENSOR,
+    Platform.WEATHER,
+]
