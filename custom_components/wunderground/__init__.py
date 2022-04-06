c06a99b27b91439e29c201e6887ce1cbb21bd296
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2022-04-06 13:42:43 -0500

    Update __init__.py

--- custom_components/wunderground/__init__.py
+++ custom_components/wunderground/__init__.py
@@ -37,6 +37,19 @@ PRESSUREUNIT = 4
 RATE = 5
 PERCENTAGEUNIT = 6
 
+async def async_setup_entry(
+    hass: core.HomeAssistant, entry: config_entries.ConfigEntry
+) -> bool:
+    """Set up platform from a ConfigEntry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Forward the setup to the sensor platform.
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "weather")
+    )
+    return True
+
 
 class WUndergroundData:
     """Get data from WUnderground."""
