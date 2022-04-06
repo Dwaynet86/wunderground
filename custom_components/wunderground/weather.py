4f7df78b22ae3e2ca5e087e5a63dce15f56c1ed8
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2022-04-06 18:46:29 -0500

    Update weather.py

--- custom_components/wunderground/weather.py
+++ custom_components/wunderground/weather.py
@@ -43,7 +43,7 @@ async def async_setup_entry(
         unit_system_api = 'e'
         unit_system = 'imperial'
 
-    rest = self.WUndergroundData(
+    rest = WUndergroundData(
         hass, config.get(CONF_API_KEY), pws_id, numeric_precision, unit_system_api, unit_system,
         config.get(CONF_LANG))
 
