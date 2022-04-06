78168908a98fc32e0bbb68189defea1803b7640b
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2022-04-06 00:35:32 -0500

    Update config_flow.py

--- custom_components/wunderground/config_flow.py
+++ custom_components/wunderground/config_flow.py
@@ -5,6 +5,7 @@ import voluptuous as vol
 from homeassistant import config_entries, core, exceptions
 from homeassistant.core import callback
 from .const import _LOGGER, DATA_WU_CONFIG, DOMAIN
+from homeassistant.const import CONF_API_KEY, CONF_PWS_ID
 from typing import Any, Dict, Optional
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_registry import (
